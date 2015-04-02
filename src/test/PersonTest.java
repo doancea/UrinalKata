6eafdc92118f77df503d909ebb4a7d9f92d33384
Danny Oancea <doancea@pillartechnology.com>
2015-04-01 21:58:11 -0500

    Got 1 occupied urinal and 1 available for free

--- src/test/PersonTest.java
+++ src/test/PersonTest.java
@@ -53,4 +53,17 @@ public class PersonTest {
         int urinalPosition = person.chooseUrinal(urinals);
         assertThat(urinalPosition, equalTo(1));
     }
+
+    @Test
+    public void chooseUrinal_whenThereAreTwoUrinalsAndOnlyTheFirstIsAvailable_ReturnsTheFirstUrinal() {
+        Person person = new Person();
+
+        ArrayList<Urinal> urinals = new ArrayList<Urinal>();
+        urinals.add(new Urinal(true));
+        urinals.add(new Urinal(false));
+
+        int urinalPosition = person.chooseUrinal(urinals);
+        assertThat(urinalPosition, equalTo(0));
+    }
+
 }
