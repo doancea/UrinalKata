607949c676038ea9388f4431fe07bbabaefbc035
Danny Oancea <doancea@pillartechnology.com>
2015-04-01 22:33:59 -0500

    Refactor into separate rules based methods

--- src/main/Person.java
+++ src/main/Person.java
@@ -8,24 +8,9 @@ import java.util.ArrayList;
 public class Person {
 
     public int chooseUrinal(ArrayList<Urinal> urinals) {
-        ArrayList<Urinal> availableUrinals = new ArrayList<Urinal>();
+        ArrayList<Urinal> availableUrinals = getAvailableUrinals(urinals);
 
-        for(int i = 0; i < urinals.size(); i++) {
-            Urinal urinal = urinals.get(i);
-            if(urinal.isAvailable()) {
-                availableUrinals.add(i, urinal);
-            }
-        }
-
-        ArrayList<Urinal> nonAdjacentUnavailableUrinals = new ArrayList<Urinal>();
-
-        for(Urinal urinal : availableUrinals) {
-            int index = urinals.indexOf(urinal);
-
-            if((index == urinals.size() - 1) || (urinals.get(index + 1).isAvailable())) {
-                nonAdjacentUnavailableUrinals.add(urinal);
-            }
-        }
+        ArrayList<Urinal> nonAdjacentUnavailableUrinals = getUrinalsWithAvailableNeighbor(urinals, availableUrinals);
 
         if(!nonAdjacentUnavailableUrinals.isEmpty()) {
             availableUrinals = nonAdjacentUnavailableUrinals;
@@ -36,5 +21,34 @@ public class Person {
         }
 
         return -1;
+    }
+
+    private ArrayList<Urinal> getUrinalsWithAvailableNeighbor(ArrayList<Urinal> urinals, ArrayList<Urinal> availableUrinals) {
+        ArrayList<Urinal> nonAdjacentUnavailableUrinals = new ArrayList<Urinal>();
+
+        for(Urinal urinal : availableUrinals) {
+            int index = urinals.indexOf(urinal);
+
+            if(isFurtherNeighborOccupied(urinals, index)) {
+                nonAdjacentUnavailableUrinals.add(urinal);
+            }
+        }
+        return nonAdjacentUnavailableUrinals;
+    }
+
+    private boolean isFurtherNeighborOccupied(ArrayList<Urinal> urinals, int index) {
+        return (index == urinals.size() - 1) || (urinals.get(index + 1).isAvailable());
+    }
+
+    private ArrayList<Urinal> getAvailableUrinals(ArrayList<Urinal> urinals) {
+        ArrayList<Urinal> availableUrinals = new ArrayList<Urinal>();
+
+        for(int i = 0; i < urinals.size(); i++) {
+            Urinal urinal = urinals.get(i);
+            if(urinal.isAvailable()) {
+                availableUrinals.add(i, urinal);
+            }
+        }
+        return availableUrinals;
     }
 }
